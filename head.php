6d6814ff8c98d9d1c55bdd3352d2774e932d4dc6
roddom67 <roddom67@gmail.com>
2019-06-24 11:51:33 -0300

    head

--- head.php
+++ head.php
@@ -10,7 +10,8 @@
 	<meta http-equiv="pragma" content="no-cache" />
 	<meta http-equiv="pragma-directive: no-cache">
 	<meta http-equiv="cache-directive: no-cache">
-	
+
+	<link rel="dns-prefetch" href="//fonts.googleapis.com">
 	<link href="https://fonts.googleapis.com/css?family=Noto+Sans|Open+Sans" rel="stylesheet"> 
 
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
@@ -26,7 +27,6 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
    <meta name="mobile-web-app-capable" content="yes">
    
-	<link href="https://fonts.googleapis.com/css?family=Libre+Franklin:100,200,300,400,600,700,900" rel="stylesheet">
 	<link rel="stylesheet" href="css/bootstrap.min.css?v=<?php echo $v; ?>" >
 	<link href="css/ninja-slider.css?v=<?php echo $v; ?>" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="css/style.css?v=<?php echo $v; ?>" >
